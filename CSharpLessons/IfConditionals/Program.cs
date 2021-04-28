8d04c6afbb535b1b26e49bfcf2829cfac96c3a9e
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-28 10:15:18 -0500

    Add files via upload

---
+++ CSharpLessons/IfConditionals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IfConditionals
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int grade = 93;
+
+            if (grade>70)
+            {
+                Console.WriteLine("You passed the class");
+                Console.ReadLine();
+            }
+            
+            
+            
+            else
+            {
+                Console.WriteLine("You failed the class");
+                Console.ReadLine();
+            }
+        }
+    }
+}
