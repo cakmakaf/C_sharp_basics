d96e47a0406bef69ef2304f60d638a4e8ce9a97a
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-28 15:49:00 -0500

    Add files via upload

---
+++ CSharpLessons/MultipTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultipTable
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine(i+"x"+j+"="+(i*j));
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
