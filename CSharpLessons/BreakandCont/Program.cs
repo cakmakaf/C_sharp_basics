96ee9a9cc0c9c0409b2c51abfd9d3f4141d2c08e
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-28 15:36:49 -0500

    Add files via upload

---
+++ CSharpLessons/BreakandCont/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BreakandCont
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i==5)
+                {
+                    continue;
+                }
+                Console.WriteLine("the value is " + i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
