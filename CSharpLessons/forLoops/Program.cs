f8887ded2b568598074c6ba1c0edd9b9cfeaf54f
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-28 13:55:07 -0500

    Add files via upload

---
+++ CSharpLessons/forLoops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace forLoops
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+            
+
+            for (int i = 0; i <= 10; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            */
+
+            for (int i = 1; i <=10 ; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
