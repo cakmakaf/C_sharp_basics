182321d4be50ecaa6520e92adfbaf4814b736690
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-28 15:37:22 -0500

    Add files via upload

---
+++ CSharpLessons/whileLoops/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace whileLoops
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*int x = 3;
+
+            while (x <= 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+            Console.ReadLine();
+            
+            int x = 300;
+
+            while (x >= 100)
+            {
+                Console.WriteLine(x);
+                x *= 5;
+            }
+
+            Console.ReadLine();
+            
+
+            int x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x<=10);
+
+            Console.ReadLine();
+            */
+
+            int x = 1;
+
+            int sum = 0;
+
+            do
+            {
+                sum += x;
+                x++;
+            } while (x<=100);
+            
+            Console.WriteLine("The total is: "+ sum);
+
+            Console.ReadLine();
+
+        }
+    }
+}
