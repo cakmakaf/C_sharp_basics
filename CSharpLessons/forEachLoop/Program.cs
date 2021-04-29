89fad5c5094e0c1e41b7cc01f5569139dcaac5f7
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-29 01:30:46 -0500

    Add files via upload

---
+++ CSharpLessons/forEachLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace forEachLoop
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // string[] fruits = {"Apple", "Banana", "Coconut", "Date Fruit", "Elderberry"};
+            //
+            // foreach (var item in fruits)
+            // {
+            //     Console.WriteLine(item);
+            // }
+            
+            double[] calories = {95.34, 105.20, 10405.18, 414.5, 99.78};
+
+            foreach (var x in calories)
+            {
+                Console.WriteLine(x);
+            }
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
