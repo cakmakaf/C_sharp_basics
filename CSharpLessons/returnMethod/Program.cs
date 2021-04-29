5a59e12065a73f868a5f9be87a36db8b869022a2
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-29 03:31:23 -0500

    Add files via upload

---
+++ CSharpLessons/returnMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace returnMethod
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int returnValue = makeSum(3, 4);
+
+
+            if (returnValue>5)
+            {
+                Console.WriteLine("The result is higher then or equal 5" );
+            }
+            else
+            {
+                Console.WriteLine("The result is less than or equal 4" );
+            }
+            
+        }
+
+        static int makeSum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+
+            return sum;
+        }
+    }
+}
