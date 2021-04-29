f19abdba02b264e3613c818a98df2466af7ddf6c
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-29 02:48:49 -0500

    Add files via upload

---
+++ CSharpLessons/Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Salut();
+            PrintName();
+            
+        }
+
+        static void Salut()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static void PrintName()
+        {
+            Console.WriteLine("Ahmet C");
+            Console.ReadLine();
+        }
+        
+    }
+}
