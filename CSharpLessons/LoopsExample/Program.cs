710ec7725d83182be44077ef01775a58bb852264
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-29 00:07:46 -0500

    Add files via upload

---
+++ CSharpLessons/LoopsExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoopsExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                sum += i;
+            }
+
+            double average = sum / num;
+
+            if (average >= 80)
+            {
+                Console.WriteLine("The average score is higher than 80...");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("The average is less than 80");
+                Console.ReadLine();
+            }
+        }
+    }
+}
