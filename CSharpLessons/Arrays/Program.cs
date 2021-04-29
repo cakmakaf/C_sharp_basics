822d8ff62f40f33e967b118ff17ba9d9d85795a6
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-29 00:19:21 -0500

    Add files via upload

---
+++ CSharpLessons/Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] nums = new int[4];
+
+            nums[0] = 1;
+            nums[1] = 11;
+            nums[2] = 21;
+            nums[3] = 31;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
