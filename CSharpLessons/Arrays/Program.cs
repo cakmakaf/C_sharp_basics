152fd9e6203f62488737024856105f0dfebd5976
Ahmet Faruk Çakmak <ahmetfarukcakmak@gmail.com>
2021-04-29 00:41:57 -0500

    Add files via upload

--- CSharpLessons/Arrays/Program.cs
+++ CSharpLessons/Arrays/Program.cs
@@ -7,19 +7,50 @@ namespace Arrays
         public static void Main(string[] args)
         {
 
-            int[] nums = new int[4];
+            // int[] nums = new int[4];
+            //
+            // nums[0] = 1;
+            // nums[1] = 11;
+            // nums[2] = 21;
+            // nums[3] = 31;
+            //
+            // for (int i = 0; i < nums.Length; i++)
+            // {
+            //     Console.WriteLine(nums[i]);
+            // }
+            //
+            // Console.ReadLine();
 
-            nums[0] = 1;
-            nums[1] = 11;
-            nums[2] = 21;
-            nums[3] = 31;
+            string[] fruits = {"Apple", "Banana", "Coconut", "Date Fruit", "Elderberry"};
 
-            for (int i = 0; i < nums.Length; i++)
+            double[] calories = {95.34, 105.20, 10405.18, 414.5, 99.78};
+
+            char[] vitamins = {'A', 'B', 'C', 'D', 'E'};
+            
+            
+            for (int i = 0; i < fruits.Length; i++)
             {
-                Console.WriteLine(nums[i]);
+                Console.WriteLine(fruits[i]);
             }
-
             Console.ReadLine();
+            
+            
+            
+            for (int i = 0; i < calories.Length; i++)
+            {
+                Console.WriteLine(calories[i]);
+            }
+            Console.ReadLine();
+            
+            
+
+            for (int i = 0; i < vitamins.Length; i++)
+            {
+                Console.WriteLine(vitamins[i]);
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
